133cc364b87e00d166bcb254c7e17227472df477
fridchikn24 <10899472+fridchikn24@users.noreply.github.com>
2023-12-10 18:41:35 -0600

    Add files via upload

--- fake_news.py
+++ fake_news.py
@@ -33,10 +33,10 @@ def preprocess(text, join_back=True):
     return result
 
 
-df['filered_text']=df['news'].apply(preprocess)
+df['filtered_text']=df['news'].apply(preprocess)
 
 
-X_train, X_test, y_train, y_test = train_test_split(df['filered_text'], df['label'], test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(df['filtered_text'], df['label'], test_size=0.2, random_state=42)
 
 tfidf_vectorizer = TfidfVectorizer(max_features=5000)
 X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
@@ -79,3 +79,28 @@ y_pred = classifier.predict(X_test_tfidf)
 
 print("Accuracy:", accuracy_score(y_test, y_pred))
 print("\nClassification Report:\n", classification_report(y_test, y_pred))
+
+
+model = Sequential()
+model.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(302, 1)))
+model.add(MaxPooling1D(pool_size=2))
+model.add(Conv1D(filters=128, kernel_size=3, activation='relu'))
+model.add(MaxPooling1D(pool_size=2))
+model.add(Conv1D(filters=256, kernel_size=3, activation='relu'))
+model.add(MaxPooling1D(pool_size=2))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))  # 1 neuron for binary classification
+
+# Compile the model
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Train the model
+model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_test, y_test))
+
+model_pred = model.predict(X_test_tfidf)
+
+
+print("Accuracy:", accuracy_score(y_test, model_pred))
+print("\nClassification Report:\n", classification_report(y_test, model_pred))
